4f33147c2d691b57f87300fc56dd0b691594d752
Alexander Holm <alho0014@student.hv.se>
2022-10-23 12:59:47 +0200

    .gitignore för server

--- server/dist/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Emojis = void 0;
-var Emojis;
-(function (Emojis) {
-    Emojis["Yes"] = "yes";
-    Emojis["No"] = "no";
-    Emojis["Write"] = "write";
-})(Emojis = exports.Emojis || (exports.Emojis = {}));
