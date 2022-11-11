2941b9bb570ca3af0d9bedc28654e9ec4c91ae90
Alexander Holm <alho0014@student.hv.se>
2022-11-11 17:06:53 +0100

    Flyttat emoji-selection till Canvas.svelte

--- client/src/components/canvas/Canvas.svelte
+++ client/src/components/canvas/Canvas.svelte
@@ -1,40 +1,48 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import EmojiButtons from "./EmojiButtons.svelte";
     import { ICONS } from "./icons"
     import { Emojis } from "@server/enums"
     import { Action } from "@server/actions";
     import { Relays } from "@server/relays"
     import { CanvasElement } from "./CanvasElement";
+    import EmojiButton from "./EmojiButton.svelte";
 
     export let socket: WebSocket;
-    console.log(ICONS[Emojis.Yes])
-
-    let canvas: HTMLCanvasElement;
-    let selectedIcon: Emojis | null = null;
     socket.addEventListener("message", receiveMessage);
 
-    // Lägger element i in[] och out[] för att köra transitions på det,
-    // när det läggs till eller tas bort från canvas
+    let canvas: HTMLCanvasElement;
+
+    let selectedEmoji: Emojis | null = null;
+    function clickEmoji(clickedEmoji: string){
+        console.log(clickedEmoji in Emojis)
+        if(clickedEmoji === selectedEmoji){
+            selectedEmoji = null;
+            return;
+        }
+        selectedEmoji = clickedEmoji as Emojis;
+    } 
+
+    // Lägger element i in[] och out[] för att köra transitions 
+    // på det när det läggs till eller tas bort från canvas
     class CanvasElements { 
         in: CanvasElement[] = [];
         static: CanvasElement[] = [];
         out: CanvasElement[] = [];
     }
     let canvasElements = new CanvasElements();
-    // Tid som ett element visas på canvas mellan transitions
+    // Tid som ett element visas på canvas mellan transitions i ms
     const ELEMENT_TIMER = 5000;
 
-    // onMount för att canvas inte ska vara undefined.
-    onMount(() => {
-        /* 
-            Storleken på canvas sätts i procent av CSS.
-            Det ändrar inte den faktiska storleken på canvas utan sträcker bara ut den.
-            Måste därför med Javascript sätta storleken till det värde i px som CSS räknar ut.
+    
+    /* 
+        Storleken på canvas sätts i procent av CSS.
+        Det ändrar inte den faktiska storleken på canvas utan sträcker bara ut den.
+        Måste därför med Javascript sätta storleken till det värde i px som CSS räknar ut.
 
-            https://stackoverflow.com/questions/10214873/make-canvas-as-wide-and-as-high-as-parent
-            Phrogz answered Apr 18, 2012 at 18:34 
-        */
+        https://stackoverflow.com/questions/10214873/make-canvas-as-wide-and-as-high-as-parent
+        Phrogz answered Apr 18, 2012 at 18:34 
+    */
+    onMount(() => {
         function resizeHandler() {
             canvas.width = canvas.offsetWidth;
             canvas.height = canvas.offsetHeight;
@@ -48,13 +56,13 @@
         if(canvas == null) return;
         
         requestAnimationFrame(() => {            
-            let ctx = canvas.getContext("2d");
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            let context = canvas.getContext("2d");
+            context.clearRect(0, 0, canvas.width, canvas.height);
 
             canvasElements.in.forEach(item => {
                 item.opacity += 0.1;
                 if(item.opacity < 1){
-                    item.draw(ctx);
+                    item.draw(context);
                 }
                 else{
                     item.opacity = 1;
@@ -74,28 +82,28 @@
             })
     
             canvasElements.static.forEach(item => {
-                item.draw(ctx)
+                item.draw(context)
             })
     
             canvasElements.out.forEach(item => {
                 item.opacity -= 0.1;
                 if(item.opacity > 0){
-                    item.draw(ctx);
+                    item.draw(context);
                 }
                 else {
                     canvasElements.out = canvasElements.out.filter(x => x !== item);
                 }            
             })
             
+            // Uppdatera canvas nästa frame om det finns element som har pågående transition,
+            // behöver inte måla om canvas ifall det endast finns static elements.
             if(canvasElements.in.length > 0 || canvasElements.out.length > 0)
                 drawCanvas();
         });
-
     }
 
-
     function sendMessage(event: MouseEvent){
-        if(selectedIcon == null) return;
+        if(selectedEmoji == null) return;
 
         const canvasRect = canvas.getBoundingClientRect();
         // left och top är avstånd från canvas till kanten av fönstret
@@ -104,15 +112,15 @@
         const xPercent = clickX / canvasRect.width;
         const yPercent = clickY / canvasRect.height;
 
-        const payload = Action.CanvasMessage( { emoji: selectedIcon, xPercent, yPercent } );
+        const payload = Action.CanvasMessage( { emoji: selectedEmoji, xPercent, yPercent } );
         socket.send(payload);
-        selectedIcon = null;
+        selectedEmoji = null;
     }
     function receiveMessage(websocketMessage: MessageEvent<any>){
         const { type, data} = JSON.parse(websocketMessage.data);
         if(type !== Relays.CanvasMessage) return;
         
-        const { emoji, xPercent, yPercent, username } = data
+        const { emoji, xPercent, yPercent, username } = data;
 
         const svg = ICONS[emoji];
         const img = new Image();        
@@ -120,14 +128,27 @@
         img.onload = () => {
             const newItem = new CanvasElement(username, img, xPercent, yPercent);
             canvasElements.in = [...canvasElements.in, newItem];
-            drawCanvas()
+            drawCanvas();
         }
     }   
 </script>
 
 <section id="canvas" class="grid">
     <canvas bind:this={canvas} on:click={sendMessage} />
-    <EmojiButtons bind:selected={selectedIcon} />
+    <div id="emoji-selection">
+        <h3>Send an emoji on the screen</h3>
+        <div class="buttons">
+            {#each Object.entries(ICONS) as [key, value], index}
+                <EmojiButton 
+                    active={selectedEmoji === key} 
+                    borderRotation={index * 90} 
+                    on:click={() => clickEmoji(key)} 
+                >
+                    {@html value}
+                </EmojiButton>
+            {/each}
+        </div>
+    </div>
 </section>
 
 <style>
@@ -166,4 +187,30 @@
                 background:white;
             }
         }
+
+
+    #emoji-selection{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }    
+    h3{
+        font-size: 2.5rem;
+        font-family: dash;
+
+        color: transparent;
+        background: linear-gradient( rgb(131, 221, 249) 20%, rgb(135, 80, 255) 60%, #e852f4 60%, rgb(254, 216, 255) 80% );
+        background-clip: text;
+        filter: drop-shadow(3px 5px #0f0037);
+
+        /* gradient blir fel om texten är mer än en rad */
+        white-space: nowrap;
+    }
+    .buttons{
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 3rem;
+    }
 </style>
